1f297e0ae3b1ae64a4c9f1f56404e7a6213996c9
Alex Dillhoff <alex.dillhoff@mavs.uta.edu>
2017-04-11 10:49:00 -0500

    Added precision metric

--- human36m/main.py
+++ human36m/main.py
@@ -45,6 +45,7 @@ def main():
                 mean=[0.00094127, 0.00060294, 0.0005603],
                 std=[0.02102633, 0.01346872, 0.01251619]
             )
+	]))
 
     train_loader = torch.utils.data.DataLoader(a, batch_size=args.batch_size,
             shuffle=True)
@@ -63,6 +64,8 @@ def train(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
 
     # Switch to train mode
     model.train()
@@ -74,15 +77,17 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         input = input.cuda()
         target = target.cuda()
-        input, target = Variable(input), Variable(target)
+        input_var, target_var = Variable(input), Variable(target)
 
         # Compute output
-        output = model(input)
-        loss = criterion(output, target)
+        output = model(input_var)
+        loss = criterion(output, target_var)
 
         # Record loss
-        # TODO: Measure accuracy
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
         losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
 
         # Compute gradient and run optimizer
         optimizer.zero_grad()
@@ -94,11 +99,29 @@ def train(train_loader, model, criterion, optimizer, epoch):
         end = time.time()
 
         if batch_idx % args.print_freq == 0:
-            print('Epoch: {0} [{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+            print('Epoch: {0} [{1}/{2}]  '
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})  '
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})  '
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                 epoch, batch_idx, len(train_loader),
-                batch_time=batch_time, data_time=data_time, loss=losses))
+                batch_time=batch_time, data_time=data_time, loss=losses,
+                top1=top1, top5=top5))
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
